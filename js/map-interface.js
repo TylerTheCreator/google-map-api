f91b10988eaef00b217045442040a05686b6c176
Guest User <student@epicodus.com>
2016-03-17 11:08:08 -0700

    add marker based on search.

--- js/map-interface.js
+++ js/map-interface.js
@@ -32,7 +32,7 @@ $(document).on('click', '.pan-to-marker', function(e) {
     var $index = $(this).data('marker-index');
     var $lat = $(this).data('marker-lat');
     var $lng = $(this).data('marker-lng');
-    if ($index != undefined) {
+    if ($index !== undefined) {
         // using indices
         var position = map.markers[$index].getPosition();
         lat = position.lat();
@@ -66,15 +66,40 @@ $(document).ready(function(){
 
     var template = $('#edit_marker_template').text();
 
-    var content = template.replace(/{{index}}/g, index).replace(/{{lat}}/g, lat).replace(/{{lng}}/g, lng);
+    var content;
+    });
 
+    $('#geocoding-form').submit(function(e){
+      e.preventDefault();
+      googleMaps.geocode({
+      address: $('#address').val().trim(),
+      callback: function(results, status) {
+        if (status == 'OK') {
+          var index = map.markers.length;
+
+          var latlng = results[0].geometry.location;
+          map.setCenter(latlng.lat(), latlng.lng());
+          map.addMarker({
+            lat: latlng.lat(),
+            lng: latlng.lng(),
+            icon: "/images/mapicon.png",
+            title: 'Marker #',
+            infoWindow: {
+              content : "<p>" +
+              "<input type='text'> " +"</p>"
+              }
+            });
+          }
+        }
+      });
+    });
     map.addMarker({
       lat: lat,
       lng: lng,
+      icon: "/images/mapicon.png",
       title: 'Marker #' + index,
       infoWindow: {
-        content : content
+        content: "<p>" + "<input type='text'>" + "</p>"
       }
     });
-  });
 });
